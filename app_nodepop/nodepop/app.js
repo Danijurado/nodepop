dc8d70a91c92035a184108b3d31ef36d21d078f8
Daniel Jurado <danieljp.85@gmail.com>
2023-10-05 00:23:35 +0200

    conectado a mongoDB

--- app_nodepop/nodepop/app.js
+++ app_nodepop/nodepop/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
+require('./lib/connectMongoose');
+
 var app = express();
 
 // view engine setup
---
+++ app_nodepop/nodepop/lib/connectMongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connection.on('error', err => {
+    console.log('Error de conexión', err);
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Conectado a mongoDB en', mongoose.connection.name);
+});
+
+mongoose.connect('mongodb://127.0.0.1/nodepop')
